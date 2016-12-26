9087d0ccd829ffe39bd0bda9fc378ef6c35c5ff5
Jared Chapman <jaredpchapman@gmail.com>
2016-12-25 19:19:01 -0700

    aliases enabled

--- shell.ml
+++ shell.ml
@@ -14,10 +14,18 @@ let code_RESET = "\x1B[0m";;
 let map fn = List.map ~f:fn;;
 let foldl fn acc xs = List.fold ~init:acc ~f:fn xs;;
 
+let history = ref ["history";"ls"];;
+
+let show lst =
+    let rec aux count = function
+        | [] -> ()
+        | x::xs -> printf "%d %s\n" count x; aux (count + 1) xs
+    in aux 1 (List.rev lst)
+;;
+
 (* List of aliases *)
 let aliases_list = ref [
     ("ls",["ls";"--color=auto"]);
-    ("ll",["ls";"-l"]);
     ("la",["ls";"-a"])
 ];;
 
@@ -34,9 +42,35 @@ let de_empty lst = foldl (fun acc x -> if x="" then acc else x::acc) [] lst |> L
 
 let tokenize s = String.split_on_chars ~on:[' ';'\t';'\n';'\r'] s |> de_empty;;
 
+
+let parse_alias lst =
+    let str = String.concat ~sep:" " lst in
+    let stripped = String.strip ~drop:(fun c -> c = '"') str in
+    String.split ~on:' ' stripped
+;;
+
+(* aliases_list := ("lx",["ls";"-l"])::!aliases_list; *)
+let alias = function
+    | [] -> printf ""
+    | "alias"::[] -> printf "Invalid alias\n"
+    | "alias"::k::"="::v ->
+        let value = parse_alias v in
+        if value = [""]
+        then printf "Invalid alias\n"
+        else aliases_list := (k,value)::!aliases_list
+    | "alias"::x::xs -> (match String.split ~on:'=' x with
+        | [] -> printf "Invalid alias\n"
+        | [""] -> printf "Invalid alias\n"
+        | y::[] -> printf "Invalid alias\n"
+        | y::ys -> aliases_list := (y,parse_alias (ys@xs))::!aliases_list)
+    | _ -> printf "Bad command\n"
+;;
+
 (* Executes a command, using build in functions for cd and exit *)
 let execute = function
     | [] -> printf ""
+    | "history"::[] -> show !history
+    | ("alias"::_) as cmd -> alias cmd
     | "exit"::_ -> exit 0
     | "cd"::d::_ -> Sys.chdir d
     | "cd"::[] -> Sys.chdir (match Sys.getenv "HOME" with Some x -> x | None -> "/")
@@ -62,11 +96,13 @@ let rec shell_loop () =
     let s = getCommand () in
     let tokens = tokenize s in
     let cmdlst = aliases tokens !aliases_list in
+    history := s::!history;
     execute cmdlst;
     shell_loop ();
 ;;
 
 let () =
+    aliases_list := ("ll",["ls";"-l"])::!aliases_list;
     Sys.catch_break true;
     print_string "Welcome to JSH (Jared SHell)\n";
     shell_loop ();
