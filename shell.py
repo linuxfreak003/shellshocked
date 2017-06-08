7b44a5f00de01ec88ec375963222c8a89f7e7e0c
Jared Chapman <jaredpchapman@gmail.com>
2017-06-07 21:07:44 -0600

    python shell started

--- shell.py
+++ shell.py
@@ -1,7 +1,49 @@
-import sys
+import sys, os
+
+KNRM = "\x1B[0m"
+KRED = "\x1B[31m"
+KGRN = "\x1B[32m"
+KYEL = "\x1B[33m"
+KBLU = "\x1B[34m"
+KMAG = "\x1B[35m"
+KCYN = "\x1B[36m"
+KWHT = "\x1B[37m"
+
+
+history = []
+
+def execute(args):
+    if args[0] == "exit":
+        return 0
+    if args[0] == "cd" and len(args) > 1:
+        os.chdir(args[1])
+        return 1
+    if args[0] == "cd":
+        os.chdir(os.getenv("HOME") )
+        return 1
+    if args[0] == "history":
+        for i in range(len(history)):
+            print(i,history[i])
+        return 1
+
+
+def shell_loop():
+    status = 1
+    while(status != 0):
+        cwd = os.getcwd()
+        command = input("%s%s$ %s" %(KCYN, cwd, KNRM) )
+        if command.strip() == "exit":
+            status = 0;
+        history.append(command)
+        args = command.split()
+        status = execute(args)
 
 def main():
-    pass
+    # Load Config files
+
+    status = shell_loop()
+
+    #Cleanup, if needed
 
 if __name__ == "__main__":
     main()
